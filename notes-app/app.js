bec30ec029a0cf45a03f59947c4f502bb37f215d
Ratika Rastogi <ratikarastogi1996@gmail.com>
2025-06-25 18:38:25 +0530

    notes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,67 +1,58 @@
-import {addNote, removeNote,listNotes,readNote} from './notes.js'
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers';
+import { addNote, removeNote, listNotes, readNote } from "./notes.js";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import eslint from "prettier-eslint";
 
+yargs(hideBin(process.argv)).command(
+  "add",
+  "Add a note",
+  {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Add body for the note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  (argv) => {
+    addNote(argv.title, argv.body);
+  },
+).argv;
 
-yargs(hideBin(process.argv))
-    .command(
-        'add',
-        'Add a note', 
-        {
-            title:{
-                describe:'Note Title',
-                demandOption:true,
-                type:'string'
-            },
-            body:{
-                describe:'Add body for the note',
-                demandOption:true,
-                type:'string'
-            }
-        },
-        (argv) => {
-           addNote(argv.title,argv.body)
-        }
-    ).argv
+yargs(hideBin(process.argv)).command(
+  "remove",
+  "Removes a note",
+  {
+    title: {
+      describe: "Note title to be removed",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  (argv) => {
+    removeNote(argv.title);
+  },
+).argv;
 
-yargs(hideBin(process.argv))
-    .command(
-        'remove', 
-        'Removes a note', 
-        {
-            title:{
-                describe:'Note title to be removed',
-                demandOption:true,
-                type:'string'
-            }
-        }, 
-        (argv) => {
-            removeNote(argv.title)
-        }
-    ).argv  
+yargs(hideBin(process.argv)).command("list", "Lists the notes", {}, () => {
+  listNotes();
+}).argv;
 
-yargs(hideBin(process.argv))
-    .command(
-        'list', 
-        'Lists the notes', 
-        {}, 
-        () => {
-            listNotes()
-        }
-    ).argv  
-
-yargs(hideBin(process.argv))
-    .command(
-        'read', 
-        'Reads a note', 
-        {
-            title:{
-                describe:'Enter the note to be read',
-                demandOption:true,
-                type:'string'
-            }
-        },
-        (argv) => {
-            readNote(argv.title)
-        }
-    ).argv    
+yargs(hideBin(process.argv)).command(
+  "read",
+  "Reads a note",
+  {
+    title: {
+      describe: "Enter the note to be read",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  (argv) => {
+    readNote(argv.title);
+  },
+).argv;
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,63 +1,62 @@
-import fs from 'node:fs'
-import chalk from 'chalk'
+import fs from "node:fs";
+import chalk from "chalk";
+import eslint from "prettier-eslint";
 
-export  function addNote(title, body){
-    const notes = loadNotes()
-    const duplicateNotes = notes.find( (note) => note.title === title)
-    if(!duplicateNotes){
-        notes.push({
-            title:title,
-            body:body
-        })
-        saveNotes(notes)        
-        console.log(chalk.green("Data Saved successfully"))
-    }else{
-        console.log(chalk.red('OOps '+title+' note already exists'))
-    }
-    
+export function addNote(title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.find((note) => note.title === title);
+  if (!duplicateNotes) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log(chalk.green("Data Saved successfully"));
+  } else {
+    console.log(chalk.red("OOps " + title + " note already exists"));
+  }
 }
 
-export function removeNote(noteTitle){
-    const notes = loadNotes()
-    const notesToKeep = notes.filter( (note) =>  note.title!==noteTitle)
-    if(notesToKeep.length === notes.length){
-        console.log(chalk.bgRed("Note not found!!!!"))
-    }else{
-        saveNotes(notesToKeep)
-        console.log(chalk.bgGreen('Note '+noteTitle+' deleted successfully!!!!'))
-    }
+export function removeNote(noteTitle) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter((note) => note.title !== noteTitle);
+  if (notesToKeep.length === notes.length) {
+    console.log(chalk.bgRed("Note not found!!!!"));
+  } else {
+    saveNotes(notesToKeep);
+    console.log(
+      chalk.bgGreen("Note " + noteTitle + " deleted successfully!!!!"),
+    );
+  }
 }
 
-export function listNotes(){
-    const notes = loadNotes()
-    console.log(chalk.bgCyanBright("Your Notes-"))
-    let i=0
-    notes.forEach((note) => console.log("<"+(++i)+">"+note.title+"\n"));
+export function listNotes() {
+  const notes = loadNotes();
+  console.log(chalk.bgCyanBright("Your Notes-"));
+  let i = 0;
+  notes.forEach((note) => console.log("<" + ++i + ">" + note.title + "\n"));
 }
 
-export function readNote(noteTitle){
-    const notes = loadNotes()
-    const note = notes.find((note) => note.title === noteTitle)
-    if(note){
-        console.log(chalk.bgGreen.bold(note.title)+"-"+ note.body)
-    }else{
-        console.log(chalk.red.bold.underline(noteTitle+' not found'))
-    }
+export function readNote(noteTitle) {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === noteTitle);
+  if (note) {
+    console.log(chalk.bgGreen.bold(note.title) + "-" + note.body);
+  } else {
+    console.log(chalk.red.bold.underline(noteTitle + " not found"));
+  }
 }
 
-const saveNotes= (notes) => {
-    const notesData = JSON.stringify(notes)
-    fs.writeFileSync('notes.json',notesData)
-}
+const saveNotes = (notes) => {
+  const notesData = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", notesData);
+};
 
-const loadNotes = () =>{
-    try{
-        const notesBuffer = fs.readFileSync('notes.json')
-         return JSON.parse(notesBuffer.toString())
-    }catch(e){
-        return []
-    }
-}
-
- 
-
+const loadNotes = () => {
+  try {
+    const notesBuffer = fs.readFileSync("notes.json");
+    return JSON.parse(notesBuffer.toString());
+  } catch (e) {
+    return [];
+  }
+};
