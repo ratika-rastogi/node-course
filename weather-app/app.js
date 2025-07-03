cedf2db9132be95bbe2601ff8929771299af9f35
Ratika Rastogi <ratikarastogi1996@gmail.com>
2025-07-03 12:04:08 +0530

    refactoring

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,18 +3,18 @@ import { forecast } from './utils/forecast.js'
 import chalk from 'chalk'
 
 
-const location = process.argv[2]
-if(location){
-    console.log("location input parameter"+location)
-    geocode(location , (geocodeError,geocodeData) => {
+const address = process.argv[2]
+if(address){
+    console.log("address input parameter"+address)
+    geocode(address , (geocodeError,{latitude,longitude,place} = {}) => {
     if(geocodeError){
        return  console.log(chalk.red('Error:'), chalk.red(error))
     }
-    forecast(geocodeData.longitude,geocodeData.latitude, (forecastError,forecastData) => {
+    forecast(latitude,longitude, (forecastError,forecastData) => {
         if(forecastError){
-            return console.log("Forecast Error:",error)
+            return console.log("Forecast Error:",forecastError)
         }
-        console.log(chalk.bgGreen('In '+ forecastData?.location.name +' it is '+ forecastData?.current.temp_c + " degree celsius. The probability of rain is "+forecastData?.current.precip_in))    
+        console.log(chalk.bgGreen('In '+ place +' it is '+ forecastData?.current.temp_c + " degree celsius. The probability of rain is "+forecastData?.current.precip_in))    
     })
     
 })
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -3,17 +3,17 @@ import request from 'postman-request'
 export function geocode  (address,callback) {
     const url = 'https://api.mapbox.com/search/geocode/v6/forward?q='+ encodeURIComponent(address) +'&access_token=pk.eyJ1IjoicmF0aWthcmFzdG9naTEiLCJhIjoiY21jZDR4Z2JoMGVtMjJpczN1OWRkNTQ5ayJ9.qM1aWyHM-KC0r3cngw8RpQ&limit=1'
     
-    request({url : url, json:true}, (error, response) => {
+    request({url, json:true}, (error, {body}) => {
         if(error){
             callback('Unable to coonect to the geocode service!!',undefined)
-        } else if (response.body.error_code || response.body.features.length === 0){
+        } else if (body.error_code || body.features.length === 0){
             callback('Unable to find location for the given data try again......',undefined)
         } else {
-            console.log("from geocode"+response.body.features[0].properties.full_address)
+            console.log("from geocode"+body.features[0].properties.full_address)
             callback(undefined,{
-                latitude:response.body.features[0].properties.coordinates.latitude,
-                longitude:response.body.features[0].properties.coordinates.longitude,
-                place:response.body.features[0].properties.full_address
+                latitude:body.features[0].properties.coordinates.latitude,
+                longitude:body.features[0].properties.coordinates.longitude,
+                place:body.features[0].properties.full_address
             })
         }
     })
