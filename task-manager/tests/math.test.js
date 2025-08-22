812249672cd7a94ca886a035e01e5e6349995702
Ratika Rastogi <ratikarastogi1996@gmail.com>
2025-08-22 18:54:21 +0530

    use jest for testing

---
+++ task-manager/tests/math.test.js
@@ -0,0 +1,41 @@
+import { calculateTip , fahrenheitToCelsius , celsiusToFahrenheit,add} from "../src/math.js";
+
+
+test('Should calculate total for the tip',()=>{
+    const total = calculateTip(10,.5)
+    expect(total).toBe(15)
+})
+
+test('Should calculate total for the default tip',()=>{
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
+})
+
+test('Should convert 32 F to 0 C',()=>{
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('Should convert 0 C to 32 F ',()=>{
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
+})
+
+// test('Async Demo',(done)=>{
+//     setTimeout(()=>{
+//         expect(1).toBe(2)
+//         done()
+//     },2000)    
+// })
+
+test('Should add 2 numbers',(done)=>{
+    add(2,3).then((sum)=>{
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should add 2 numbers - using async/await', async ()=>{
+    const sum = await add(10,20)
+    expect(sum).toBe(30)
+})
