a38be5d6135c50eaf088f0f8db490044d90f83f3
Ratika Rastogi <ratikarastogi1996@gmail.com>
2025-08-29 17:33:15 +0530

    Add tests for users

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,17 +1,6 @@
-import express from 'express'
-import { connect } from './db/mongoose.js'
-import { userRouter } from './routers/users.js'
-import { taskRouter } from './routers/tasks.js'
-connect()
+import {app} from './app.js'
 
-const app = express()
 const port = process.env.PORT
-
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
 
 app.listen(port, ()=>{
     console.log('Server listening on port -',port)
---
+++ task-manager/tests/user.test.js
@@ -0,0 +1,94 @@
+import request from 'supertest'
+import app from '../src/app.js'
+import { User } from '../src/db/models/users.js'
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne={
+    _id:userOneId,
+    name:'Test User',
+    email:'test@example.com',
+    password:'passCode!!',
+    tokens:[{
+        token: jwt.sign({_id:userOneId},process.env.JWT_SECRET)
+    }]
+}
+
+beforeEach(async ()=>{
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('should sign up a new user', async () =>{
+    const response = await request(app).post('/users').send({
+        name:'Shashank Rastogi',
+        email:'shashankrastogi125@gmail.com',
+        password:'shashank@123'
+    }).expect(201)
+
+    // Assert that database changes are done correctly
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    //Assertions about the response
+    expect(response.body).toMatchObject({
+        user:{
+             name:'Shashank Rastogi',
+             email:'shashankrastogi125@gmail.com',
+        },
+        token: user.tokens[0].token
+    })
+
+    expect(user.password).not.toBe('shashank@123')
+})
+
+test('Should login existing user',async ()=>{
+    const response = await request(app).post('/users/login').send({
+        email:userOne.email,
+        password:userOne.password
+    }).expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
+})
+
+test('Should not login non-existent user',async ()=>{
+    await request(app).post('/users/login').send({
+        email:'testing@failure.com',
+        password:'failure!!'
+    }).expect(400)
+})
+
+test('Should get profile for user',async ()=>{
+    await request(app)
+        .get('/users/me')
+        .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get profile for unauthenticated user',async ()=>{
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Should delete account for user',async ()=>{
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    
+    const user = await User.findById(userOneId)    
+    expect(user).toBeNull()
+})
+
+test('Should not delete account for unauthenticated user',async ()=>{
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
+})
