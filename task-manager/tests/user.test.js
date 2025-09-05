ea71eabadd37a0b61bc5ede4eff8cc0d3d420c89
Ratika Rastogi <ratikarastogi1996@gmail.com>
2025-09-05 14:13:54 +0530

    user tests wind up

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -92,3 +92,31 @@ test('Should not delete account for unauthenticated user',async ()=>{
         .send()
         .expect(401)
 })
+
+test('Should upload avatar image',async () =>{
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar','tests/fixtures/website.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))    
+})
+
+test('Should update valid user fields',async()=>{
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+        .send({name:'Shashank'})
+        .expect(200)
+    const user = await User.findById(userOneId) 
+    expect(user.name).toBe('Shashank')   
+})
+
+test('Should not update invalid user fields',async()=>{
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+        .send({location:'India'})
+        .expect(404)  
+})
